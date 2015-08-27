3ec977e18253b619483d15e7d982cb8c79665df2
theaaa <thea53309@gmail.com>
2015-08-27 17:15:41 +0000

    changes

--- models/model.rb
+++ models/model.rb
@@ -1,9 +1,16 @@
 users = ["User1", "User2","User3", "User4", "User5"]
-puts users.sample(2)
-
+def user_names
+        return users.sample(2)
+end
 adjectives = ["aggressive","arrogant","boisterous","boring","crazy","compassionate","depressed","disdainful","flamboyant","glamorous","jovial","klutzy","miserable","neat","nifty","puzzled","prudent","quirky","self-concious","sexy","sneaky","suspicious","vapid","violent","wimpy"]
-puts adjectives.sample(1)
-
+def adjectives
+        return adjectives.sample(1)
+end
 vegetables = ["asparagus", "artichoke","arugala","beetroot","bok choy","brussel sprouts","cabbage","carrots","cauliflower","celery","collard greens","cucumber","kale","leek","lettuce","onion","potato","radish","scallion","shallot","sqaush","spinach","sweet potato","turnip","zuccini"]
-puts vegetables.sample(1)
+def vegetables
+        return vegetables.sample(1)
+end
 
+puts user_names
+puts adjectives
+puts vegetables
