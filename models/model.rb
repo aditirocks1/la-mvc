b9551de222c4c46f945a01a7ec2939fd0883bedf
aditirocks1 <aditi.patil0324@gmail.com>
2015-08-27 18:40:43 +0000

    hi

--- models/model.rb
+++ models/model.rb
@@ -1,17 +1,20 @@
-users = ["User1", "User2","User3", "User4", "User5"]
-def user_names
-    return users.sample(2)
+def user_names(users)
+   return users.to_a.sample(2)
 end
-adjectives = ["aggressive","arrogant","boisterous","boring","crazy","compassionate","depressed","disdainful","flamboyant","glamorous","jovial","klutzy","miserable","neat","nifty","puzzled","prudent","quirky","ratchet", "self-concious","sexy","sneaky","suspicious","violent","wimpy"]
+
 def adjectives
+    adjectives = ["aggressive","arrogant","boisterous","boring","crazy","compassionate","depressed","disdainful","flamboyant","glamorous","jovial","klutzy","miserable","neat","nifty","puzzled","prudent","quirky","ratchet","self-concious","sexy","sneaky","suspicious","violent","wimpy"]
     return adjectives.sample(1)
 end
-vegetables = ["asparagus", "artichoke","arugala","beetroot","bok choy","brussel sprouts","cabbage","carrots","cauliflower","celery","collard greens","cucumber","kale","leek","lettuce","onion","potato","radish","scallion","shallot","squash","spinach","sweet potato","turnip","zuccini"]
+
 def vegetables
+    vegetables = ["asparagus", "artichoke","arugala","beetroot","bok choy","brussel sprouts","cabbage","carrots","cauliflower","celery","collard greens","cucumber","kale","leek","lettuce","onion","potato","radish","scallion","shallot","squash","spinach","sweet potato","turnip","zuccini"]
     return vegetables.sample(1)
 end
 
-puts user_names
+
+puts user_names(users)
 puts adjectives
 puts vegetables
+
 
