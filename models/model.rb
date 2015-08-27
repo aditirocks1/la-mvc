45df6ee579042a26e6f911b3e951f9e33f02faf4
aditirocks1 <aditi.patil0324@gmail.com>
2015-08-27 16:57:43 +0000

    erb

--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,9 @@
 users = ["User1", "User2","User3", "User4", "User5"]
 puts users.sample(2)
 
+adjectives = ["aggressive","arrogant","boisterous","boring","crazy","compassionate","depressed","disdainful","flamboyant","glamorous","jovial","klutzy","miserable","neat","nifty","puzzled","prudent","quirky","self-concious","sexy","sneaky","suspicious","vapid","violent","wimpy"]
+puts adjectives.sample(1)
 
+vegetables = ["asparagus", "artichoke","arugala","beetroot","bok choy","brussel sprouts","cabbage","carrots","cauliflower","celery","collard greens","cucumber","kale","leek","lettuce","onion","potato","radish","scallion","shallot","sqaush","spinach","sweet potato","turnip","zuccini"]
+puts vegetables.sample(1)
 
